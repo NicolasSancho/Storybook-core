92c7e35561254a5c59911207d026a94b4ef87b78
NicolasSancho <sanchonicolas@hotmail.com>
2024-08-23 11:20:40 +0200

    Trying to compile tailwind on the build

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './styles/tailwind.css';
 export * from './components/Button';
 export * from './components/Header';
 export * from './components/Page';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './.storybook/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./.storybook/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
